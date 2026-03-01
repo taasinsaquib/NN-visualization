9f88edd9ce905c6583f09aa3d92cd6dd38d63565
taasinsaquib <taasin.saquib@gmail.com>
2026-03-01 15:07:11 -0800

    vite base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: '/alexnet_visualization/',
+    base: '/NN-visualization/',
 });
