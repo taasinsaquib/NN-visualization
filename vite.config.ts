2ad5430500f00d3eeae3b274a9dc64f6cb484feb
taasinsaquib <taasin.saquib@gmail.com>
2026-03-01 15:00:33 -0800

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/alexnet_visualization/',
+  base: '/NN-visualization/',
 })
